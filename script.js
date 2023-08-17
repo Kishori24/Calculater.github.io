c3b2f2e159484dce53c0d07d3ae08a589fd536d4
Kishori24 <131950610+Kishori24@users.noreply.github.com>
2023-08-17 16:48:01 +0530

    first. commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentDisplay = "";
+
+function appendToDisplay(value) {
+    currentDisplay += value;
+    document.getElementById("display").value = currentDisplay;
+}
+
+function clearDisplay() {
+    currentDisplay = "";
+    document.getElementById("display").value = currentDisplay;
+}
+
+function calculate() {
+    try {
+        currentDisplay = eval(currentDisplay).toString();
+        document.getElementById("display").value = currentDisplay;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
+
+
+
+
